518a893588359eeefbddd673b91f7e7adf35a2d5
dukim <dukim@dacon.kr>
2026-05-05 11:44:01 +0900

    :sparkles: 4장 token-bucket-demo 프로젝트 스캐폴드 추가

---
+++ 04_처리율제한장치설계/token-bucket-demo/src/main/java/kr/study/tokenbucket/TokenBucketDemoApplication.java
@@ -0,0 +1,11 @@
+package kr.study.tokenbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenBucketDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TokenBucketDemoApplication.class, args);
+    }
+}
